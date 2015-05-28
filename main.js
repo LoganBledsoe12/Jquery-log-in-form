795f6b4dd94a5f81fa587a8544b3c13b9195c2aa
Logan Bledsoe <lbledsoe12@hotmail.com>
2015-05-28 18:18:00 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+var $txtname = $('#txtname');
+var $spnname = $('#spnname');
+
+var $txtemail = $('#txtemail');
+var $spnemail = $('#spnemail');
+
+var $txtsite = $('#txtsite');
+var $spnsite = $('#spnsite');
+
+var $txtmessage = $('#txtmessage');
+var $spnmessage = $('#spnmessage');
+
+
+var $btnsubmit = $('#btnsubmit');
+
+
+$btnsubmit.on('click', submit);
+$btnsubmit.on('mouseover', colorChange);
+$btnsubmit.on('mouseout', changeBack);
+$btnsubmit.on('keypress', clickEnter);
+$spnname.hide();
+$spnemail.hide();
+$spnsite.hide();
+$spnmessage.hide();
+
+function submit (){
+	if($txtname.val()==''){
+		$spnname.show();
+	}
+	if($txtemail.val().indexOf ('@') === -1){
+		$spnemail.show();
+	}
+	if ($txtsite.val()==''){
+		$spnsite.show();
+	}
+	if ($txtsite.val().indexOf ('http://') === -1){
+		$spnsite.show();
+	}
+	if ($txtmessage.val()==''){
+		$spnmessage.show();
+	}
+}
+function colorChange(){
+	$btnsubmit.css ('background-Color', '#88C470');
+}
+function changeBack(){
+	$btnsubmit.css('background-color', 'rgb(25,26,27)')
+}
+function clickEnter(e){
+	if(e.which == 13){
+		submit();
+	}
+}
+
+
+
